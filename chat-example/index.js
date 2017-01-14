ee9a7401ac1dcd709fc7304be4843c1f8e7e7a41
ccblazer44 <ccblazer44@gmail.com>
2017-01-14 13:52:54 -0700

    added room support

--- chat-example/index.js
+++ chat-example/index.js
@@ -6,10 +6,20 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
+// io.on('connection', function(socket){
+//   socket.on('chat message', function(msg){
+//     io.emit('chat message', msg);
+//   });
+// });
+
 io.on('connection', function(socket){
+
+  socket.join('some room2');
+
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    io.to('some room2').emit('chat message', msg);
   });
+
 });
 
 http.listen(3000, function(){
